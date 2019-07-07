f995e4482396e5a8b3c4faa989e674b9d8c4846b
linxiaofeng1314 <1013755465@qq.com>
2019-07-07 19:17:50 +0800

    首页完成

--- player/src/App.vue
+++ player/src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <div id="app">
+  <div id="player">
+    <h2 class="title">黑云音乐</h2>
+    <div class="search">
+      <input type="text"  />
+      <button >
+        <span class="iconfont icon-search"></span>
+      </button>
+    </div>
+    <div class="tab-wrapper">
+      <!-- tab栏 -->
+      <div class="tab-bar">
+        <!-- 搜索结果 -->
+        <router-link to="/results" class="bar-item active">搜索结果</router-link>
 
+        <a class="bar-item">歌曲播放</a>
+        <a class="bar-item">mv</a>
+        <a class="bar-item">歌曲评论&gt;</a>
+        <a class="bar-item">推荐歌曲</a>
+      </div>
+      <!-- 对应的内容区域 -->
+      <div class="tab-content">
+        <router-view>
+
+        </router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name:"app",
-  
-}
+  name: "app"
+};
 </script>
 
 <style>
-
 </style>
--- player/src/main.js
+++ player/src/main.js
@@ -3,18 +3,32 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入样式
+// 首页样式
+import './assets/css/index.css'
+import './assets/css/iconfont.css'
+// 搜索样式
+import './assets/css/results.css'
+
 // 导入路由
 import VueRouter from 'vue-router'
+
+// 导入results组件
+import result from './components/results.vue'
+
 // 设置use
-vue.use(VueRouter)
+Vue.use(VueRouter)
+
 // 路由规则
 const routes = [
-  
+  {path:'/results',component:result}
 ]
+
 // 路由对象
 const router = new VueRouter({
   routes
 })
+
 // 设置给vue实例
 new Vue({
   render: h => h(App),
